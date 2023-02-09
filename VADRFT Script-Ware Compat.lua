a5b4f15c469e15ecf9d302fb0bbe9690c4dc24cd
Xulaari <dabadiedabadie269@gmail.com>
2023-02-09 16:20:40 +0000

    Hopefully fixed all systems.

--- VADRFT Script-Ware Compat.lua
+++ VADRFT Script-Ware Compat.lua
@@ -23,8 +23,6 @@ if not syn then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/zzerexx/scripts/main/SynapseToScriptWare.lua",true))()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AnthonyIsntHere/anthonysrepository/main/scripts/AntiChatLogger.lua",true))()
   loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/scripts/main/vadriftsbyp'),true))()
-  local Alert = library:Notification("Vadrift Launcher", "Script-Ware Compatibility Is Launching.", 5, Color3.fromRGB(255, 255, 255))
-  Alert:Button("Button")
 else
   loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/scripts/main/vadriftsbyp'),true))()
 end
