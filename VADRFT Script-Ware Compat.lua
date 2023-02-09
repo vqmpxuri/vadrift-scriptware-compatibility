f6abd15ecf12f4dfa769c61bcd0b11e698fda970
Xulaari <dabadiedabadie269@gmail.com>
2023-02-09 16:18:35 +0000

    Lines switched.

--- VADRFT Script-Ware Compat.lua
+++ VADRFT Script-Ware Compat.lua
@@ -22,13 +22,13 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TrillyX/Resources/main/NFLib"))()
 
 if not syn then
-  local Alert = library:Notification("Vadrift Launcher", "Script-Ware Compatibility Is Launching.", 5, Color3.fromRGB(255, 255, 255))
-Alert:Button("Button")
   loadstring(game:HttpGet("https://raw.githubusercontent.com/zzerexx/scripts/main/SynapseToScriptWare.lua",true))()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AnthonyIsntHere/anthonysrepository/main/scripts/AntiChatLogger.lua",true))()
   loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/scripts/main/vadriftsbyp'),true))()
+  local Alert = library:Notification("Vadrift Launcher", "Script-Ware Compatibility Is Launching.", 5, Color3.fromRGB(255, 255, 255))
+  Alert:Button("Button")
 else
-  local Alert = library:Notification("Vadrift Launcher", "Synapse Version Is Loading.", 5, Color3.fromRGB(255, 255, 255))
-Alert:Button("Button")
   loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/scripts/main/vadriftsbyp'),true))()
+  local Alert = library:Notification("Vadrift Launcher", "Synapse Version Is Loading.", 5, Color3.fromRGB(255, 255, 255))
+  Alert:Button("Button")
 end
